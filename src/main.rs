2f8d788d496158a79556407fe0f5f8e1ee3194fb
teki.sei <teki.sei@uzabase.com>
2019-09-01 02:10:10 +0900

    request_with_async_await

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#![feature(async_closure)]
+
 use failure::Fail;
-use futures::{future, Future};
+use futures::executor::block_on;
+use futures01::{future, Future};
 use reqwest::r#async::Client as AsyncClient;
 use reqwest::{Client, Url};
 use std::thread;
@@ -22,14 +25,15 @@ impl From<reqwest::Error> for ErrorWrapper {
 }
 
 fn main() {
-    if true {
-        request_by_multi_thread()
-    } else {
-        request_by_main_thread()
+    let target = 1;
+    match target {
+        1 => request_with_multi_thread(),
+        2 => block_on(request_with_async_await()), // this is not a parallel request example
+        _ => request_with_main_thread(),
     }
 }
 
-fn request_by_main_thread() {
+fn request_with_main_thread() {
     output1("START SINGLE THREAD");
     let start_at = SystemTime::now();
 
@@ -47,7 +51,14 @@ fn request_by_main_thread() {
     output1(format!("END: {}", spent_time.as_millis()).as_str())
 }
 
-fn request_by_multi_thread() {
+fn send_request(client: &Client) -> ResultWithError<String> {
+    let mut response = client
+        .get(Url::parse("http://localhost:9000/timestamp").unwrap())
+        .send()?;
+    Ok(response.text()?)
+}
+
+fn request_with_multi_thread() {
     output1("START MULTI THREAD");
     let start_at = SystemTime::now();
 
@@ -58,9 +69,9 @@ fn request_by_multi_thread() {
     let mut handles = Vec::<SpawnHandle<(ThreadId, String), ErrorWrapper>>::new();
     while handles.iter().count() <= 1000 {
         let cloned_client = client.clone();
-        handles.push(
-            thread_pool.spawn_handle(future::lazy(move || send_request_async(&cloned_client))),
-        );
+        handles.push(thread_pool.spawn_handle(future::lazy(move || {
+            send_request_for_future(&cloned_client)
+        })));
     }
 
     handles.iter_mut().for_each(|handle| match handle.wait() {
@@ -74,14 +85,7 @@ fn request_by_multi_thread() {
     output1(format!("END: {}", spent_time.as_millis()).as_str())
 }
 
-fn send_request(client: &Client) -> ResultWithError<String> {
-    let mut response = client
-        .get(Url::parse("http://localhost:9000/timestamp").unwrap())
-        .send()?;
-    Ok(response.text()?)
-}
-
-fn send_request_async(
+fn send_request_for_future(
     client: &AsyncClient,
 ) -> impl Future<Item = (ThreadId, String), Error = ErrorWrapper> {
     client
@@ -90,6 +94,33 @@ fn send_request_async(
         .and_then(|mut response| response.text())
         .map(|text| (thread::current().id(), text))
         .from_err()
+}
+
+async fn request_with_async_await() {
+    output1("START ASYNC AWAIT");
+    let start_at = SystemTime::now();
+
+    let client = Client::new();
+    let future1 = send_request_async(&client);
+    let future2 = send_request_async(&client);
+    match future1.await {
+        Ok((thread_id, text)) => output2(thread_id, text.as_str()),
+        Err(error) => output1(format!("{:?}", error).as_str()),
+    };
+    match future2.await {
+        Ok((thread_id, text)) => output2(thread_id, text.as_str()),
+        Err(error) => output1(format!("{:?}", error).as_str()),
+    };
+
+    let spent_time = start_at.elapsed().unwrap_or(Duration::new(0, 0));
+    output1(format!("END: {}", spent_time.as_millis()).as_str())
+}
+
+async fn send_request_async(client: &Client) -> ResultWithError<(ThreadId, String)> {
+    let mut response = client
+        .get(Url::parse("http://localhost:9000/timestamp").unwrap())
+        .send()?;
+    Ok((thread::current().id(), response.text()?))
 }
 
 fn output2(thread_id: ThreadId, text: &str) {
